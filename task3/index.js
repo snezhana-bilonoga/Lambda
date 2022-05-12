1e6f82c8d2f67c27b6839dd623447534668aab13
Snezhana <snezhana.bilonoga16@gmail.com>
2022-05-12 16:20:33 +0300

    task added

---
+++ task3/index.js
@@ -0,0 +1,24 @@
+const { developers } = require('./data');
+const cleanData = developers.map((developer) => {
+    const userId = developer.user._id;
+    const name = developer.user.name;
+    const startDate = developer.startDate;
+    const endDate = developer.endDate;
+    const weekendDates = { startDate, endDate };
+
+    return { userId, name, weekendDates };
+});
+
+const aggData = cleanData.reduce((acc, curr) => {
+    const value = acc.find((el) => el.userId === curr.userId);
+
+    if (value) {
+        value.weekendDates.push(curr.weekendDates);
+    } else {
+        curr.weekendDates = [curr.weekendDates];
+        acc.push(curr);
+    }
+
+    return acc;
+}, []);
+console.log(aggData);
